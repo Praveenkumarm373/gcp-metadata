a91424de42c4094205c6f32f993069109660f993
Praveenkumarm373 <45892375+Praveenkumarm373@users.noreply.github.com>
2019-06-26 15:18:46 +0530

    Update gcp.sh

--- gcp.sh
+++ gcp.sh
@@ -6,7 +6,6 @@ sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_
 sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication  yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 echo 'zippyops' | passwd --stdin root
-echo '35.239.170.248 instance-1.us-central1-a.c.chef-244409.internal instance-1' >> /etc/hosts
 ip=$(curl icanhazip.com)
 echo "ssh-keygen -R $ip" > /home/bootstrap.sh
 echo "knife bootstrap $ip -x root -P zippyops -N node" >> /home/bootstrap.sh
